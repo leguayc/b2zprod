c467550f905efa6d008b374da65f9b2ebc51f150
Paul Marechal <paulmarechal785@hotmail.fr>
2022-06-13 17:58:25 +0200

    Add click on three obj

--- assets/app/components/scene3d.svelte
+++ assets/app/components/scene3d.svelte
@@ -115,22 +115,64 @@ onMount(() => {
     // //   updateCameraTweens(cameraSettings);
     // });
 
-    cameraMove.addEventListener('click', function() {
-        console.log("click camera")
+    // cameraMove.addEventListener('click', function() {
+    //     console.log("click camera")
+    //     gsap.to(camera.position, {z: -1.8, duration: 10});
+    //     gsap.to(camera.position, {x: 1.2, duration: 10});
+    //     gsap.to(camera.position, {y: -1.5, duration: 2});
+    //     controls.target.set( -12, -2, -2);
+    //     camera.rotation.z = 10
+    // });
+    function cameraMove(){
         gsap.to(camera.position, {z: -1.8, duration: 10});
         gsap.to(camera.position, {x: 1.2, duration: 10});
         gsap.to(camera.position, {y: -1.5, duration: 2});
         controls.target.set( -12, -2, -2);
         camera.rotation.z = 10
-    });
+    }
+
+    // Welcome button 
+    const raycaster = new THREE.Raycaster()
+
+    const geometryPlane = new THREE.PlaneGeometry( 1, 1 );
+    const materialPlane = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+    const plane = new THREE.Mesh( geometryPlane, materialPlane );
+    plane.position.set(0, -2.4, 26.5)
+    plane.name = "plane";
+    scene.add( plane );
+
+    window.addEventListener('click', onDocumentMouseDown, false);
+
+    var mouse = new THREE.Vector2();
+    function onDocumentMouseDown( event ) {
+        mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+        raycaster.setFromCamera( mouse, camera );
+        var intersects = raycaster.intersectObjects( scene.children );
+        
+        if ( intersects.length > 0 ) {
+            console.log(intersects[0].object.name)
+            intersects[0].object.callback();
+        }
+    }
+
+    plane.callback = function() { cameraMove();}
+    // circle2.callback = function() { changeByPortal();}
+
+    // function changeByPortal(){
+    //     var selectedObject = scene.getObjectByName("cercle"); 
+    //     const darkmode =  new Darkmode();
+    //     darkmode.toggle();
+    // }
+
 
     // GUI tests
     gui.add(cube.position, 'x').min(-60).max(60).step(0.01).name('cube X')
     gui.add(cube.position, 'y').min(-60).max(60).step(0.01).name('cube Y')
     gui.add(cube.position, 'z').min(-60).max(60).step(0.01).name('cube Z')
-    gui.add(camera.position, 'x').min(-60).max(60).step(0.01).name('Camera X')
-    gui.add(camera.position, 'y').min(-60).max(60).step(0.01).name('Camera Y')
-    gui.add(camera.position, 'z').min(-60).max(60).step(0.01).name('Camera Z')
+    gui.add(plane.position, 'x').min(-60).max(60).step(0.01).name('plane X')
+    gui.add(plane.position, 'y').min(-60).max(60).step(0.01).name('plane Y')
+    gui.add(plane.position, 'z').min(-60).max(60).step(0.01).name('plane Z')
 
     // Controls
     console.log(canvas)
@@ -181,7 +223,7 @@ onMount(() => {
 </script>
 
 <!-- <button bind:this={button1} style="position:relative;" id="button1" class="camera-button" >Position 1</button> -->
-<button bind:this={cameraMove} style="position:relative;">Camera</button>
+<!-- <button bind:this={cameraMove} style="position:relative;">Camera</button> -->
 <canvas bind:this={canvas} class="webgl"></canvas>
 
 
