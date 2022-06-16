da6a3c27f8b2d7468681e9d98ee5226db1c3e1cb
Paul Marechal <paulmarechal785@hotmail.fr>
2022-06-17 00:40:17 +0200

    pictures posters / remove gui helpers

--- assets/app/components/scene3d.svelte
+++ assets/app/components/scene3d.svelte
@@ -15,7 +15,7 @@ onMount(() => {
     const camera = SceneHelper.addCamera(scene);
     SceneHelper.addLights(scene);
     
-    ModelHelper.createTestCube(scene);
+    // ModelHelper.createTestCube(scene);
 
     ModelHelper.createPoster(scene, '../posters/affiche.png', {x: -5.6, y: -2.08, z: 2.41}, 1.59, "posterAffiche", cameraMoveCine);
     ModelHelper.createPoster(scene, '../posters/infos.png', {x: -5.6, y: -2.08, z: 0.11}, 1.59, "posterInfos", cameraMoveCine);
@@ -24,31 +24,31 @@ onMount(() => {
     ModelHelper.createPoster(scene, '../posters/contact.png', {x: -5.6, y: -2.08, z: -6.63}, 1.59, "posterContact", cameraMoveContact);
 
     ModelHelper.createPoster(scene, '../posters/contact.png', {x: 13.2, y: -1.4, z: -5.74}, -1.575, "posterContact", cameraMoveContact, {width: 1.18, height: 3.35});
-    ModelHelper.createPoster(scene, '../posters/infos.png', {x: 13.2, y: -1.96, z: -1.96}, -1.575, "posterContact", cameraMoveContact, {width: 1.18, height: 3.2});
-    ModelHelper.createPoster(scene, '../posters/talents.png', {x: 13.2, y: -0.04, z: -4.05}, -1.575, "posterContact", cameraMoveContact, {width: 1.18, height: 3.35});
-    const poster3 = ModelHelper.createPoster(scene, '../posters/affiche.png', {x: 13.2, y: -0.26, z: -2.28}, -1.58, "posterContact", cameraMoveContact, {width: 1.18, height: 3.35});
-    const grandPoster = ModelHelper.createPoster(scene, '../posters/affiche.png', {x: 11.5, y: -1.3, z: -6.9}, 0, "posterContact", cameraMoveContact, {width: 1.93, height: 5.32});
-    const grandPosterEntree = ModelHelper.createPoster(scene, '../posters/affiche.png', {x: 4.8, y: -1.64, z: -7.95}, 0, "posterContact", cameraMoveContact, {width: 1.38, height: 3.8});
+    ModelHelper.createPoster(scene, '../posters/contactRoom/hakim.png', {x: 13.2, y: -1.96, z: -1.96}, -1.575, "posterContact", cameraMoveContact, {width: 1.18, height: 3.2});
+    ModelHelper.createPoster(scene, '../posters/contactRoom/gaetan.png', {x: 13.2, y: -0.04, z: -4.05}, -1.575, "posterContact", cameraMoveContact, {width: 1.18, height: 3.35});
+    const poster3 = ModelHelper.createPoster(scene, '../posters/contactRoom/walid.png', {x: 13.2, y: -0.26, z: -2.28}, -1.58, "posterContact", cameraMoveContact, {width: 1.6, height: 2.3});
+    const grandPoster = ModelHelper.createPoster(scene, '../posters/contactRoom/poster1.jpg', {x: 11.5, y: -1.3, z: -6.9}, 0, "posterContact", cameraMoveContact, {width: 1.93, height: 5.32});
+    const grandPosterEntree = ModelHelper.createPoster(scene, '../posters/contactRoom/poster2.jpg', {x: 4.8, y: -1.64, z: -7.95}, 0, "posterContact", cameraMoveContact, {width: 1.38, height: 3.8});
     const posterTalent = ModelHelper.createPoster(scene, '../posters/affiche.png', {x: 13.2, y: -1.9, z: -3.78}, -1.58, "posterContact", cameraMoveContact, {width: 1.18, height: 3.35});
     const poster4 = ModelHelper.createPoster(scene, '../posters/affiche.png', {x: 13.2, y: -1.25, z: -0.25}, -1.58, "posterContact", cameraMoveContact, {width: 1.18, height: 3.35});
 
     // Posters front wall entrance
-    ModelHelper.createPoster(scene, '../posters/affiche.png', {x: 5.15, y: -2.34, z: 4.45}, 0.1, "posterContact", cameraMoveContact, {width: 1.38, height: 3.8});
-    ModelHelper.createPoster(scene, '../posters/affiche.png', {x: 9.25, y: -2.33, z: 4.3}, 0, "posterContact", cameraMoveContact, {width: 1.38, height: 3.85});
-    const grandPosterEntree2 = ModelHelper.createPoster(scene, '../posters/affiche.png', {x: -9.02, y: -2.34, z: 4.2}, 0, "posterContact", cameraMoveContact, {width: 1.38, height: 3.85});
-    ModelHelper.createPoster(scene, '../posters/affiche.png', {x: -5.14, y: -2.33, z: 4.45}, -0.07, "posterContact", cameraMoveContact, {width: 1.38, height: 3.85});
+    ModelHelper.createPoster(scene, '../posters/contactRoom/poster1.jpg', {x: 5.15, y: -2.34, z: 4.45}, 0.1, "posterContact", cameraMoveContact, {width: 1.38, height: 3.8});
+    ModelHelper.createPoster(scene, '../posters/contactRoom/poster2.jpg', {x: 9.25, y: -2.33, z: 4.3}, 0, "posterContact", cameraMoveContact, {width: 1.38, height: 3.85});
+    const grandPosterEntree2 = ModelHelper.createPoster(scene, '../posters/contactRoom/poster2.jpg', {x: -9.02, y: -2.34, z: 4.2}, 0, "posterContact", cameraMoveContact, {width: 1.38, height: 3.85});
+    ModelHelper.createPoster(scene, '../posters/contactRoom/poster1.jpg', {x: -5.14, y: -2.33, z: 4.45}, -0.07, "posterContact", cameraMoveContact, {width: 1.38, height: 3.85});
     
-    poster3.rotation.x = 1.58
+    // poster3.rotation.x = 1.58
     poster4.rotation.x = 1.58
     posterTalent.rotation.x = 1.58;
 
-    // const cubeTestRotation = SceneHelper.gui.addFolder('floor ')
-    //     cubeTestRotation.add(floor.position, 'x').min(-60).max(60).step(0.01).name('position X')
-    //     cubeTestRotation.add(floor.position, 'y').min(-60).max(60).step(0.01).name('position Y')
-    //     cubeTestRotation.add(floor.position, 'z').min(-60).max(60).step(0.01).name('position Z')
-    //     cubeTestRotation.add(floor.rotation, 'x').min(-60).max(60).step(0.01).name('rotation X')
-    //     cubeTestRotation.add(floor.rotation, 'y').min(-60).max(60).step(0.01).name('rotation Y')
-    //     cubeTestRotation.add(floor.rotation, 'z').min(-60).max(60).step(0.01).name('rotation Z')
+    // const cubeTestRotation = SceneHelper.gui.addFolder('poster3 ')
+    //     cubeTestRotation.add(poster3.position, 'x').min(-60).max(60).step(0.01).name('position X')
+    //     cubeTestRotation.add(poster3.position, 'y').min(-60).max(60).step(0.01).name('position Y')
+    //     cubeTestRotation.add(poster3.position, 'z').min(-60).max(60).step(0.01).name('position Z')
+    //     cubeTestRotation.add(poster3.rotation, 'x').min(-60).max(60).step(0.01).name('rotation X')
+    //     cubeTestRotation.add(poster3.rotation, 'y').min(-60).max(60).step(0.01).name('rotation Y')
+    //     cubeTestRotation.add(poster3.rotation, 'z').min(-60).max(60).step(0.01).name('rotation Z')
 
     /**
      * Click function
@@ -58,13 +58,13 @@ onMount(() => {
     // Return buttons 
     ModelHelper.createButton(scene, "", 0, {x: 0, y: -2.4, z: 26.5}, "plane", cameraMoveReturn);
     ModelHelper.createButton(scene, "", 0, {x: -3.1, y: -0.1, z: -12.5}, "return", cameraMoveReturn);
-    const returnButton1 = ModelHelper.createButton(scene, "", 1.5, {x: 10.1, y: -0.07, z: -5.55}, "return1", cameraMoveReturn);
+    const returnButton1 = ModelHelper.createButton(scene, "", 1.5, {x: 11.1, y: -0.07, z: -5.55}, "return1", cameraMoveReturn);
 
     // GUI tests
-    const cubeTest = SceneHelper.gui.addFolder('posterAffiche ');
-    cubeTest.add(posterTalent.position, 'x').min(-60).max(60).step(0.01).name('position X');
-    cubeTest.add(posterTalent.position, 'y').min(-60).max(60).step(0.01).name('position Y');
-    cubeTest.add(posterTalent.position, 'z').min(-60).max(60).step(0.01).name('position Z');
+    // const cubeTest = SceneHelper.gui.addFolder('posterAffiche ');
+    // cubeTest.add(posterTalent.position, 'x').min(-60).max(60).step(0.01).name('position X');
+    // cubeTest.add(posterTalent.position, 'y').min(-60).max(60).step(0.01).name('position Y');
+    // cubeTest.add(posterTalent.position, 'z').min(-60).max(60).step(0.01).name('position Z');
 
     /**
      * Movie screen (test with local video)
@@ -83,10 +83,10 @@ onMount(() => {
     scene.add(videoScreen);
 
     // GUI panel 
-    const planeTest = SceneHelper.gui.addFolder('camera');
-    planeTest.add(camera.position, 'x').min(-60).max(60).step(0.01).name('position X');
-    planeTest.add(camera.position, 'y').min(-60).max(60).step(0.01).name('position Y');
-    planeTest.add(camera.position, 'z').min(-60).max(60).step(0.01).name('position Z');
+    // const planeTest = SceneHelper.gui.addFolder('camera');
+    // planeTest.add(camera.position, 'x').min(-60).max(60).step(0.01).name('position X');
+    // planeTest.add(camera.position, 'y').min(-60).max(60).step(0.01).name('position Y');
+    // planeTest.add(camera.position, 'z').min(-60).max(60).step(0.01).name('position Z');
 
     /**
      * Text 
@@ -116,15 +116,15 @@ onMount(() => {
         // test disposition1 
         text.position.set(-3.2, 1.55, 7.75)
 
-        const textPos = SceneHelper.gui.addFolder('return (position)')
-        textPos.add(returnButton1.position, 'x').min(-60).max(60).step(0.01).name('position X')
-        textPos.add(returnButton1.position, 'y').min(-60).max(60).step(0.01).name('position Y')
-        textPos.add(returnButton1.position, 'z').min(-60).max(60).step(0.01).name('position Z')
+        // const textPos = SceneHelper.gui.addFolder('return (position)')
+        // textPos.add(returnButton1.position, 'x').min(-60).max(60).step(0.01).name('position X')
+        // textPos.add(returnButton1.position, 'y').min(-60).max(60).step(0.01).name('position Y')
+        // textPos.add(returnButton1.position, 'z').min(-60).max(60).step(0.01).name('position Z')
 
-        const cubeTestRotation = SceneHelper.gui.addFolder('return (rotation)')
-        cubeTestRotation.add(returnButton1.rotation, 'x').min(-60).max(60).step(0.01).name('position X')
-        cubeTestRotation.add(returnButton1.rotation, 'y').min(-60).max(60).step(0.01).name('position Y')
-        cubeTestRotation.add(returnButton1.rotation, 'z').min(-60).max(60).step(0.01).name('position Z')
+        // const cubeTestRotation = SceneHelper.gui.addFolder('return (rotation)')
+        // cubeTestRotation.add(returnButton1.rotation, 'x').min(-60).max(60).step(0.01).name('position X')
+        // cubeTestRotation.add(returnButton1.rotation, 'y').min(-60).max(60).step(0.01).name('position Y')
+        // cubeTestRotation.add(returnButton1.rotation, 'z').min(-60).max(60).step(0.01).name('position Z')
     });
 
     /**
