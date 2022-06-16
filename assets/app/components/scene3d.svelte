e86e1b457b24d5c3d68214f8c2cb5bb54e1ff09f
leguayc <90463306+leguayc@users.noreply.github.com>
2022-06-16 14:55:33 +0200

    Dev front 3 d (#11)
    * first camera movement done
    
    * Add click on three obj
    
    * poster first room
    
    * add img link - first room
    
    * added AmbientLight on scene
    
    * added movie screen and camera movement
    
    * button return movieScreenRoom - top left
    
    * added camera movement contactRoom
    
    * return button & test curve text
    
    * final scene
    
    * Refactored scene3D following code review
    
    Co-authored-by: Paul Marechal <paulmarechal785@hotmail.fr>

--- assets/app/components/scene3d.svelte
+++ assets/app/components/scene3d.svelte
@@ -1,238 +1,144 @@
 <script>
-import * as dat from 'lil-gui';
 import * as THREE from 'three';
 import { onMount } from 'svelte';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
-import { TWEEN } from 'three/examples/jsm/libs/tween.module.min';
-// import { gsap } from "gsap";
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import * as SceneHelper from "../helpers/Scene3DHelper.js";
+import * as ModelHelper from "../helpers/Model3DHelper.js";
+import { onDocumentMouseDown, cameraMoveReturn, cameraMoveCine, cameraMoveContact, setupNavigation } from "../helpers/Navigation3DHelper.js";
 
 // Canvas
 let canvas;
-let button1;
 
 onMount(() => {
+    const scene = SceneHelper.createSceneBase();
+    const sizes = SceneHelper.setResize();
+    const camera = SceneHelper.addCamera(scene);
+    SceneHelper.addLights(scene);
+    
+    ModelHelper.createTestCube(scene);
+
+    ModelHelper.createPoster(scene, '../posters/affiche.png', {x: -5.6, y: -2.08, z: 2.41}, 1.59, "posterAffiche", cameraMoveCine);
+    ModelHelper.createPoster(scene, '../posters/infos.png', {x: -5.6, y: -2.08, z: 0.11}, 1.59, "posterInfos", cameraMoveCine);
+    const posterTalent = ModelHelper.createPoster(scene, '../posters/talents.png', {x: -5.6, y: -2.08, z: -2.16}, 1.59, "posterTalent", cameraMoveCine);
+    ModelHelper.createPoster(scene, '../posters/actus1.png', {x: -5.6, y: -2.08, z: -4.41}, 1.59, "posterActus", cameraMoveCine);
+    ModelHelper.createPoster(scene, '../posters/contact.png', {x: -5.6, y: -2.08, z: -6.63}, 1.59, "posterContact", cameraMoveContact);
+
     /**
-     * Base
+     * Click function
      */
-    // Debug
-    const gui = new dat.GUI({
-        width: 400
+    window.addEventListener('click', onDocumentMouseDown, false);
+
+    // Return buttons 
+    ModelHelper.createButton(scene, "", 0, {x: 0, y: -2.4, z: 26.5}, "plane", cameraMoveReturn);
+    ModelHelper.createButton(scene, "", 0, {x: -3.1, y: -0.1, z: -12.5}, "return", cameraMoveReturn);
+    const returnButton1 = ModelHelper.createButton(scene, "", 1.5, {x: 10.1, y: -0.07, z: -5.55}, "return1", cameraMoveReturn);
+
+    // GUI tests
+    const cubeTest = SceneHelper.gui.addFolder('posterAffiche ');
+    cubeTest.add(posterTalent.position, 'x').min(-60).max(60).step(0.01).name('position X');
+    cubeTest.add(posterTalent.position, 'y').min(-60).max(60).step(0.01).name('position Y');
+    cubeTest.add(posterTalent.position, 'z').min(-60).max(60).step(0.01).name('position Z');
+
+    /**
+     * Movie screen (test with local video)
+     */
+    const video = document.getElementById('video');
+    const videoTexture = new THREE.VideoTexture(video);
+    const videoMaterial =  new THREE.MeshBasicMaterial( {map: videoTexture, side: THREE.FrontSide, toneMapped: false} );
+
+    const screen = new THREE.PlaneGeometry(10.1, 14.05*0.51);
+    const videoScreen = new THREE.Mesh(screen, videoMaterial);
+    video.play()
+
+    videoScreen.position.set(0, -2.6, -22)
+    videoScreen.name = "movieScreen"
+
+    scene.add(videoScreen);
+
+    // GUI panel 
+    const planeTest = SceneHelper.gui.addFolder('camera');
+    planeTest.add(camera.position, 'x').min(-60).max(60).step(0.01).name('position X');
+    planeTest.add(camera.position, 'y').min(-60).max(60).step(0.01).name('position Y');
+    planeTest.add(camera.position, 'z').min(-60).max(60).step(0.01).name('position Z');
+
+    /**
+     * Text 
+     */
+
+    SceneHelper.fontLoader.load('../fonts/helvetiker_regular.typeface.json', (font) => {
+        const textGeometry = new TextGeometry('B2Z Production',
+            {
+                font: font,
+                size: 0.7,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        );
+        
+        const matcapTexture = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } )
+        const textMaterial = new THREE.MeshMatcapMaterial({matcap : matcapTexture})
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+
+        scene.add(text)
+
+        // test disposition1 
+        text.position.set(-3.2, 1.55, 7.75)
+
+        const textPos = SceneHelper.gui.addFolder('return (position)')
+        textPos.add(returnButton1.position, 'x').min(-60).max(60).step(0.01).name('position X')
+        textPos.add(returnButton1.position, 'y').min(-60).max(60).step(0.01).name('position Y')
+        textPos.add(returnButton1.position, 'z').min(-60).max(60).step(0.01).name('position Z')
+
+        const cubeTestRotation = SceneHelper.gui.addFolder('return (rotation)')
+        cubeTestRotation.add(returnButton1.rotation, 'x').min(-60).max(60).step(0.01).name('position X')
+        cubeTestRotation.add(returnButton1.rotation, 'y').min(-60).max(60).step(0.01).name('position Y')
+        cubeTestRotation.add(returnButton1.rotation, 'z').min(-60).max(60).step(0.01).name('position Z')
     });
 
-    // Scene
-    const scene = new THREE.Scene()
-    scene.background = new THREE.Color( "#fff" );
     /**
-     * Loaders
+     * Controls
      */
-    // Texture loader
-    const textureLoader = new THREE.TextureLoader()
-
-    // Draco loader
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderPath('draco/')
-
-    // GLTF loader
-    const gltfLoader = new GLTFLoader()
-    gltfLoader.setDRACOLoader(dracoLoader)
-
-    /**
-     * textures
-     */
-    const bakedTexture = textureLoader.load('baked.jpg')
-    bakedTexture.flipY = false
-
-    /**
-     * Materials
-     */
-    // Baked material 
-    const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
-
-    /**
-     * Model
-     */
-    gltfLoader.load(
-        'cinema.glb', 
-        (gltf) => {
-            gltf.scene.traverse((child) => {
-                child.material = bakedMaterial
-            })
-            console.log(gltf.scene)
-            scene.add(gltf.scene)
-        }
-    )
-
-    /**
-     * Sizes
-     */
-    const sizes = {
-        width: window.innerWidth,
-        height: window.innerHeight
-    }
-
-    window.addEventListener('resize', () =>
-    {
-        // Update sizes
-        sizes.width = window.innerWidth
-        sizes.height = window.innerHeight
-
-        // Update camera
-        camera.aspect = sizes.width / sizes.height
-        camera.updateProjectionMatrix()
-
-        // Update renderer
-        renderer.setSize(sizes.width, sizes.height)
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    })
-
-    /**
-     * Camera
-     */
-    // Base camera
-    const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
-    camera.position.x = 0
-    camera.position.y = 3.2
-    camera.position.z = 60
-    scene.add(camera)
-
-
-
-
-
-
-    // test deplacement camera - button 
-
-    var camPos = new THREE.Vector3(0, 0, 0);       // Holds current camera position
-    var targetPos = new THREE.Vector3(10, 10, -10);// Target position
-    var origin = new THREE.Vector3(0, 0, 0);       // Optional origin
-
-    function animate(){
-        // Interpolate camPos toward targetPos
-        camPos.lerp(targetPos, 0.05);
-
-        // USE GSAP
-
-        // Apply new camPos to your camera
-        camera.position.copy(camPos);
-
-        // (Optional) have camera look at the origin after it's been moved
-        camera.lookAt(origin);
-
-        // render();
-    }
-
-    // test 2
-
-    var buttonCameraSettings = {
-        button1: {
-        position: {x: 10, y: 0, z: 0},
-        rotation: {x: 0, y: Math.PI, z: 0}
-        }
-    };
-
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    button1.addEventListener('click', function(ev) {
-      var buttonId = ev.target.id;
-      var cameraSettings = buttonCameraSettings[buttonId];
-
-      animate()
-
-    //   updateCameraTweens(cameraSettings);
-    });
-
-    function updateCameraTweens(params) {
-      if (params.position) {
-        positionTween.stop();
-        positionTween.to(params.position, 1000).start();
-      }
-
-      if (params.rotation) {
-        rotationTween.stop();
-        rotationTween.to(params.rotation, 1000).start();
-      }
-    }
-
-    var quatTween = new TWEEN.Tween(camera.quaternion);
-    var toQuaternion = new THREE.Quaternion();
-    var toEuler = new THREE.Euler();
-
-    // in updateCameraTweens()
-    // toEuler.set(rotation.x, rotation.y, rotation.z);
-    // toQuaternion.setFromEuler(toEuler);
-    // quatTween.to(toQuaternion, 1000).start();
-
-
-    // fin test 
-
-
-
-
-
-
-
-    // Controls
-    console.log(canvas)
-    const controls = new OrbitControls(camera, canvas)
-
-    controls.enableDamping = true
-    controls.dampingFactor = 0.05
-    controls.screenSpacePanning = false
-    controls.minDistance = 2
-    controls.maxDistance = 30
-
-    controls.maxPolarAngle = (Math.PI / 2) - 0.1;
-
-    gui.add(camera.position, 'x').min(-20).max(60).step(0.01).name('Camera X')
-    gui.add(camera.position, 'y').min(-20).max(60).step(0.01).name('Camera Y')
-    gui.add(camera.position, 'z').min(-20).max(60).step(0.01).name('Camera Z')
-
-    console.log(canvas)
+    const controls = SceneHelper.createControls(canvas);
 
     /**
      * Renderer
      */
-    const renderer = new THREE.WebGLRenderer({
-        canvas: canvas,
-        antialias: true
-    })
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    const renderer = SceneHelper.createRenderer(canvas, sizes);
+    
+    setupNavigation(scene, renderer, camera, controls);
 
     /**
      * Animate
      */
-    const clock = new THREE.Clock()
+    const clock = new THREE.Clock();
 
     const tick = () =>
     {
-        const elapsedTime = clock.getElapsedTime()
-
-        // THREE.MathUtils.lerp(scene.rotation.x, Math.cos(t / 2) / 15 + 0.25 + rotationOffset[0], 0.1);
-        // camera.position.x = THREE.MathUtils.lerp(camera.position.x, 30, 0.1);
-
+        const elapsedTime = clock.getElapsedTime();
 
         // Update controls
-        controls.update()
+        controls.update();
 
         // Render
-        renderer.render(scene, camera)
+        renderer.render(scene, camera);
 
         // Call tick again on the next frame
-        window.requestAnimationFrame(tick)
-    }
+        window.requestAnimationFrame(tick);
+    };
 
-    tick()
+    tick();
 });
 
 </script>
 
-
+<!-- <button bind:this={button1} style="position:relative;" id="button1" class="camera-button" >Position 1</button> -->
+<!-- <button bind:this={cameraMove} style="position:relative;">Camera</button> -->
+<video id="video" playsinline webkit-playsinline muted loop autoplay width="2000" height="500"src="../posters/testFilm/test.mov" style="display: none;"></video>
 <canvas bind:this={canvas} class="webgl"></canvas>
-<button bind:this={button1} style="position:relative" id="button1" class="camera-button" >Position 1</button>
+
 
