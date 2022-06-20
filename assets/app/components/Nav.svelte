9b21f7a0a5cdf67dfa6738b8280dc98424656e96
Corentin Leguay <corentin.leguay@gmail.com>
2022-06-20 15:07:37 +0200

    Added better language switcher

--- assets/app/components/Nav.svelte
+++ assets/app/components/Nav.svelte
@@ -1,11 +1,22 @@
 <script>
     import { getLocalization } from '../i18n';
+    import langs from '../../../public/locales/langs.json';
     const { t, currentLanguage } = getLocalization();
 
+    const changeLang = () => {
+        currentLanguage.update(current => {
+            let allLangs = langs.allLangs;
+            for(let i = 0; i < allLangs.length; i++) {
+                if (allLangs[i] === current) {
+                    return i + 1 < allLangs.length ? allLangs[i + 1] : allLangs[0];  
+                }
+            }
+        });
+    };
 </script>
 
 <nav>
     <a href="/"><img src="/assets/images/logo.svg" alt="b2z production logo" class="logo"></a>
     <a href="/" class="contain-menu"><img src="/assets/images/menu.png" alt="menu" class="menu"></a>
-    <button on:click={() => { currentLanguage.update(current => current === 'en' ? 'fr' : 'en') }}>{$currentLanguage}</button>
+    <button on:click={changeLang}>{($currentLanguage).toUpperCase()}</button>
 </nav>
